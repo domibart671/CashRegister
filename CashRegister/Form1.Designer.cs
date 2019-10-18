1d7b11c48f3a2f453b89fefb7ab6286cb20a65f3
domibart671 <domibart671@SNS-D-130S0F>
2019-10-18 12:33:03 -0400

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace CashRegister
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.subTotalOutput = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Burgers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Fries";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Drinks";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 270);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 65);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sub Total:\r\n\r\nTax:\r\n\r\nTotal:\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(200, 344);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Print Recipt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(223, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 31);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Calculate Total";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(209, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(209, 148);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(209, 197);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox4.Location = new System.Drawing.Point(174, 31);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.Text = "Bartmanns Burgers";
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(467, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 303);
+            this.label6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(672, 64);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 303);
+            this.label7.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(467, 367);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(224, 18);
+            this.label8.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(467, 46);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(224, 18);
+            this.label9.TabIndex = 14;
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.Location = new System.Drawing.Point(284, 344);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(75, 23);
+            this.newOrderButton.TabIndex = 15;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = true;
+            this.newOrderButton.Click += new System.EventHandler(this.NewOrderButton_Click);
+            // 
+            // subTotalOutput
+            // 
+            this.subTotalOutput.Location = new System.Drawing.Point(243, 270);
+            this.subTotalOutput.Name = "subTotalOutput";
+            this.subTotalOutput.Size = new System.Drawing.Size(100, 17);
+            this.subTotalOutput.TabIndex = 16;
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.Location = new System.Drawing.Point(243, 299);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(100, 17);
+            this.taxOutput.TabIndex = 17;
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.Location = new System.Drawing.Point(245, 324);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(100, 17);
+            this.totalOutput.TabIndex = 18;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.subTotalOutput);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "form1";
+            this.Text = "Bartmanns Burgers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button newOrderButton;
+        private System.Windows.Forms.Label subTotalOutput;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalOutput;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+/// <summary>
+/// 
+/// </summary>
+namespace CashRegister
+{
+    public partial class form1 : Form
+    {
+        const double BURGER_PRICE = 5.00;
+        const double FRY_PRICE = 3.00;
+        const double DRINK_PRICE = 2.00;
+
+        double tax = 0.13;
+
+        int numOfBurgers = 0;
+        int numOfFries = 0;
+        int numOfDrink = 0;
+
+        double subTotal = 0;
+        double totalTax;
+        double totalPrice;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            numOfBurgers = Convert.ToInt16(textBox1.Text);
+            numOfFries = Convert.ToInt16(textBox2.Text);
+            numOfDrink = Convert.ToInt16(textBox3.Text);
+
+            subTotal = BURGER_PRICE * numOfBurgers + FRY_PRICE * numOfFries + DRINK_PRICE * numOfDrink;
+            totalTax = subTotal * tax;
+            totalPrice = subTotal + totalTax;
+
+            subTotalOutput.Text = subTotal.ToString("C");
+            taxOutput.Text = totalTax.ToString("C");
+            totalOutput.Text = totalPrice.ToString("C");
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Graphics Settings
+                Graphics g = this.CreateGraphics();
+                Font reciptFont = new Font("consolas", 10);
+                SolidBrush ReciptBrush = new SolidBrush(Color.Black);
+
+                g.DrawString("Bartmans Burgers", reciptFont, ReciptBrush, 490, 70);
+
+                Thread.Sleep(500);
+
+                g.DrawString("Burgers x " + numOfBurgers + "    " + BURGER_PRICE.ToString("C"), reciptFont, ReciptBrush, 490, 95);
+
+                Thread.Sleep(500);
+
+                g.DrawString("Fries x " + numOfFries + "    " + FRY_PRICE.ToString("C"), reciptFont, ReciptBrush, 490, 110);
+
+                Thread.Sleep(500);
+
+                g.DrawString("Drinks x " + numOfDrink + "    " + DRINK_PRICE.ToString("C"), reciptFont, ReciptBrush, 490, 130);
+
+                Thread.Sleep(500);
+
+                g.DrawString("Subtotal " + subTotal.ToString("C"), reciptFont, ReciptBrush, 490, 150);
+
+                Thread.Sleep(500);
+
+                g.DrawString("Tax " + totalTax.ToString("C"), reciptFont, ReciptBrush, 490, 170);
+
+                Thread.Sleep(500);
+
+                g.DrawString("Total " + totalPrice.ToString("C"), reciptFont, ReciptBrush, 490, 190);
+
+
+            }
+            catch { }
+        }
+
+        private void NewOrderButton_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+
+            g.Clear(Color.Chocolate);
+
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+
+            numOfBurgers = 0;
+            numOfFries = 0;
+            numOfDrink = 0;
+
+            subTotalOutput.Text = "";
+        }
+    }
+}
